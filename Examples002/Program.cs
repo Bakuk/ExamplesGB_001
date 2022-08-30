c52c0f345a305c866569b9cb4aa7c35c248bac70
Bakuk <bakuk1996@mail.ru>
2022-08-30 14:09:35 +0700

    add examples 002

---
+++ Examples002/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Examples002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello ");
+            Console.WriteLine(userName);
+        }
+    }
+}
